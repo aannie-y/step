22c8bb72bba879d06a644b3eb8f96454a09ebca4
Annie <annieyang0812@gmail.com>
2020-12-11 00:14:35 +0000

    change project layout - remove collapsible button

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -16,14 +16,56 @@
  * Adds a random greeting to the page.
  */
 function addRandomFact() {
-  const facts =
-      ['My childhood dream job was to be a police officer!', 'I am a huge fan of the Marvel universe!', 
-      'My favourite superhero is Iron Man!', 'I do Taekwondo', 'I love playing tennis'];
+  const facts = [
+    'My childhood dream job was to be a police officer!',
+    'I am a huge fan of the Marvel universe!',
+    'My favourite superhero is Iron Man!', 'I do Taekwondo',
+    'I love playing tennis', 'I recently started to learn tricking!'
+  ];
 
   // Pick a random greeting.
   const fact = facts[Math.floor(Math.random() * facts.length)];
 
   // Add it to the page.
-  const factContainer = document.getElementById('greeting-container');
+  const factContainer = document.getElementById('fact-container');
   factContainer.innerText = fact;
+}
+
+// sticky navbar code from w3 schools
+window.onscroll = function() {
+  stickyNav()
+};
+
+// Get navbar
+var navbar = document.getElementById('myTopNav');
+// Get offset position of navbar
+var sticky = navbar.offsetTop;
+
+// Add sticky class to navbar when you reach its scroll position. Remove
+// "sticky" when you leave scroll position
+function stickyNav() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add('sticky');
+  } else {
+    navbar.classList.remove('sticky');
+  }
+}
+
+var acc = document.getElementsByClassName('accordion');
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener('click', function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle('active');
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === 'block') {
+      panel.style.display = 'none';
+    } else {
+      panel.style.display = 'block';
+    }
+  });
 }
