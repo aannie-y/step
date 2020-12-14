33953957486ba0684d1186f28b8adeafcf1c1126
Annie <annieyang0812@gmail.com>
2020-12-14 01:21:39 +0000

    edit my first servlet

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -23,10 +23,10 @@ import javax.servlet.http.HttpServletResponse;
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
-
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
+    response.getWriter().println("<h1>Hello Annie!</h1>");
+    response.getWriter().println("<p>My first servlet!</p>");
   }
 }
