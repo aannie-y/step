c3a6532ea24e469c6f5f6e8c1fc6c4c4415bc419
Annie <annieyang0812@gmail.com>
2021-01-11 00:28:29 +0000

    change assertEquals to assertThat from truths library

--- walkthroughs/week-5-tdd/project/src/test/java/com/google/sps/FindMeetingQueryTest.java
+++ walkthroughs/week-5-tdd/project/src/test/java/com/google/sps/FindMeetingQueryTest.java
@@ -14,6 +14,8 @@
 
 package com.google.sps;
 
+import static com.google.common.truth.Truth.assertThat;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -377,12 +379,9 @@ public final class FindMeetingQueryTest {
     request.addOptionalAttendee(PERSON_A);
     request.addOptionalAttendee(PERSON_B);
 
-    Collection<TimeRange> actual = query.query(events, request);
-    Collection<TimeRange> expected =
-        Arrays.asList(TimeRange.fromStartEnd(TimeRange.START_OF_DAY, TIME_0900AM, false),
+    assertThat(query.query(events, request))
+        .containsExactly(TimeRange.fromStartEnd(TimeRange.START_OF_DAY, TIME_0900AM, false),
             TimeRange.fromStartEnd(TIME_0930AM, TIME_1000AM, false),
             TimeRange.fromStartEnd(TIME_1100AM, TimeRange.END_OF_DAY, true));
-
-    Assert.assertEquals(expected, actual);
   }
 }
